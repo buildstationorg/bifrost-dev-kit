5093f4f5c2bd618118ba5d22b02d097151f78302
ZxStim <thang.victor.luong@gmail.com>
2025-05-29 15:15:27 +0700

    feat: added YieldDelegationVault

--- src/L2Slpx/L2Slpx.sol
+++ src/L2Slpx/L2Slpx.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IVToken} from "src/L2Slpx/IVToken.sol";
 
 contract L2Slpx is Ownable {
--- src/YieldDelegationVault.sol
+++ src/YieldDelegationVault.sol
@@ -1,21 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {L2Slpx} from "src/L2Slpx/L2Slpx.sol";
 import {vDOT} from "src/L2Slpx/vDOT.sol";
 import {vETH} from "src/L2Slpx/vETH.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract YieldDelegationVault is Ownable {
+
+    /*//////////////////////////////////////////////////////////////
+                            ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error InvalidTokenAddress();
+    error InsufficientShares();
+    error NotDepositor();
+
+
+    /*//////////////////////////////////////////////////////////////
+                            EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event Deposit(address indexed user, uint256 depositId, address tokenAddress, uint256 depositedAmount, uint256 depositConversionRate);
+    event Withdraw(address indexed user, uint256 depositId, address tokenAddress, uint256 withdrawnAmount, uint256 withdrawalConversionRate);
+    event OwnerWithdrawYield(address indexed tokenAddress, uint256 amount);
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
 
     L2Slpx public immutable l2Slpx;
     vDOT public immutable vdot;
     vETH public immutable veth;
+    uint256 public currentDepositId;
+
+
+    /*//////////////////////////////////////////////////////////////
+                            STRUCTS & ENUMS
+    //////////////////////////////////////////////////////////////*/
+
+    struct VaultDepositRecord {
+        uint256 depositId;
+        address depositor;
+        address tokenAddress;
+        uint256 amountDeposited;
+        uint256 depositConversionRate;
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                            MAPPINGS
+    //////////////////////////////////////////////////////////////*/
+
+    mapping(address => uint256[]) public addressToDepositIds;
+    mapping(uint256 => VaultDepositRecord) public depositIdToDepositRecord;
+    
+
 
     /*//////////////////////////////////////////////////////////////
                             CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
+
     /// @notice Initialize the contract
     /// @param _owner The owner of the contract
     /// @param _l2Slpx The address of the L2Slpx contract
@@ -27,6 +70,7 @@ contract YieldDelegationVault is Ownable {
         veth = vETH(_vETH);
     }
 
+
     /*//////////////////////////////////////////////////////////////
                             CORE LOGIC
     //////////////////////////////////////////////////////////////*/
@@ -35,28 +79,79 @@ contract YieldDelegationVault is Ownable {
     /// @param tokenAddress The address of the token
     /// @param amount The amount of the token
     function deposit(address tokenAddress, uint256 amount) public payable {
-        if (tokenAddress == address(0)) {
-            if (msg.value < amount) revert InsufficientBalance();
-        } else {
-            if (IVToken(tokenAddress).balanceOf(msg.sender) < amount) revert InsufficientBalance();
+        if (tokenAddress != address(vdot) && tokenAddress != address(veth)) revert InvalidTokenAddress();
+        
+        if (tokenAddress == address(veth)) {
+            veth.transferFrom(msg.sender, address(this), amount);
+            uint256 depositConversionRate = l2Slpx.getTokenConversionInfo(address(veth)).tokenConversionRate;
+            addressToDepositIds[msg.sender].push(currentDepositId);
+            depositIdToDepositRecord[currentDepositId] = VaultDepositRecord({
+                depositId: currentDepositId,
+                depositor: msg.sender,
+                tokenAddress: address(veth),
+                amountDeposited: amount,
+                depositConversionRate: depositConversionRate
+            });
+            currentDepositId++;
+            emit Deposit(msg.sender, currentDepositId, address(veth), amount, depositConversionRate);
+        }
+
+        if (tokenAddress == address(vdot)) {
+            vdot.transferFrom(msg.sender, address(this), amount);
+            uint256 depositConversionRate = l2Slpx.getTokenConversionInfo(address(vdot)).tokenConversionRate;
+            addressToDepositIds[msg.sender].push(currentDepositId);
+            depositIdToDepositRecord[currentDepositId] = VaultDepositRecord({
+                depositId: currentDepositId,
+                depositor: msg.sender,
+                tokenAddress: address(vdot),
+                amountDeposited: amount,
+                depositConversionRate: depositConversionRate
+            });
+            currentDepositId++;
+            emit Deposit(msg.sender, currentDepositId, address(vdot), amount, depositConversionRate);
         }
     }
 
     /// @notice Withdraw tokens from the vault
-    /// @param tokenAddress The address of the token
-    /// @param amount The amount of the token
-    function withdraw(address tokenAddress, uint256 amount) public {
-        if (tokenAddress == address(0)) {
-            if (address(this).balance < amount) revert InsufficientBalance();
-        } else {
-            if (IVToken(tokenAddress).balanceOf(address(this)) < amount) revert InsufficientBalance();
+    /// @param depositId The id of the deposit
+    function withdraw(uint256 depositId) public {
+        VaultDepositRecord memory depositRecord = depositIdToDepositRecord[depositId];
+        if (depositRecord.depositor != msg.sender) revert NotDepositor();
+        if (depositRecord.tokenAddress == address(veth)) {
+            uint256 withdrawalConversionRate = l2Slpx.getTokenConversionInfo(address(veth)).tokenConversionRate;
+            uint256 amountOfUnderlyingDeposited = depositRecord.amountDeposited * 1e18 / depositRecord.depositConversionRate;
+            uint256 amountToWithdraw = amountOfUnderlyingDeposited * withdrawalConversionRate / 1e18;
+            veth.transfer(msg.sender, amountToWithdraw);
+        }
+        if (depositRecord.tokenAddress == address(vdot)) {
+            uint256 withdrawalConversionRate = l2Slpx.getTokenConversionInfo(address(veth)).tokenConversionRate;
+            uint256 amountOfUnderlyingDeposited = depositRecord.amountDeposited * 1e18 / depositRecord.depositConversionRate;
+            uint256 amountToWithdraw = amountOfUnderlyingDeposited * withdrawalConversionRate / 1e18;
+            vdot.transfer(msg.sender, amountToWithdraw);
+            emit Withdraw(msg.sender, depositId, depositRecord.tokenAddress, amountToWithdraw, withdrawalConversionRate);
         }
     }
 
+
+    /*//////////////////////////////////////////////////////////////
+                            INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    // TODO: Implement ERC721 logic for positions
+
+
+    /*//////////////////////////////////////////////////////////////
+                            OWNER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
     /// @notice Withdraw yield from the vault for the owner
     /// @dev This function is only callable by the owner
-    function ownerWithdrawYield() public onlyOwner {
-        l2Slpx.withdrawFees(address(vdot));
-        l2Slpx.withdrawFees(address(veth));
+    function ownerWithdrawYield(address tokenAddress) public onlyOwner {
+        if (tokenAddress != address(vdot) && tokenAddress != address(veth)) revert InvalidTokenAddress();
+        if (tokenAddress == address(veth)) {
+            veth.transfer(msg.sender, veth.balanceOf(address(this)));
+        }
+        if (tokenAddress == address(vdot)) {
+            vdot.transfer(msg.sender, vdot.balanceOf(address(this)));
+        }
     }
 }
